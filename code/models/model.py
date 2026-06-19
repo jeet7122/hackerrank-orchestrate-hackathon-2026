b5416f73818306d2fde5d87e89ec37db1397f429
jeet7122 <jeetthakkar2612@gmail.com>
2026-06-19 01:36:50 -0500

    feat: add initial data models and .gitignore

---
+++ code/models/model.py
@@ -0,0 +1,89 @@
+from dataclasses import dataclass, field
+
+"""
+############################################################################
+--------------------------------MODULE ONE----------------------------------
+############################################################################
+"""
+
+
+@dataclass
+class ClaimInput:
+    user_id: str
+    image_path: list[str]
+    user_claim: str
+    claim_object: str
+    
+@dataclass
+class UserHistory:
+    user_id: str
+    history_flags: list[str]
+    history_summary: str
+    
+@dataclass
+class EvidenceRequirement:
+    requirement_id: str
+    claim_object: str
+    applies_to: str
+    minimum_image_evidence: str
+
+@dataclass
+class ClaimTarget:
+    issue_type: str
+    object_part: str
+    prompt_injection: bool
+    
+"""
+############################################################################
+--------------------------------MODULE TWO----------------------------------
+############################################################################
+"""
+    
+    
+@dataclass
+class ImageObservation:
+    image_id: str
+    visible_object: str
+    visible_part: str
+    visible_issue: str
+    severity: str
+    valid_image: str
+    risK_flags: list[str]
+    
+
+"""
+############################################################################
+--------------------------------MODULE THREE--------------------------------
+############################################################################
+"""
+
+@dataclass
+class EvidenceAssessment:
+    evidence_standart_met: bool
+    evidence_standart_met_reason: str
+
+
+"""
+############################################################################
+--------------------------------MODULE FOUR---------------------------------
+############################################################################
+"""
+@dataclass
+class ClaimResult:
+    user_id: str
+    image_path: str
+    user_claim: str
+    claim_object: str
+    evidence_standard_met: str
+    evidence_standard_met_reason: str
+    risk_flags: str
+    issue_type: str
+    object_part: str
+    claim_status: str
+    claim_status_justification: str
+    supporting_image_ids: str
+    valid_image: str
+    severity: str
+
+
+    
